8f62e9c686f27a8f5965c3b5affc92b1c55cf6a5
mogitechglobal-oss <mogitechglobal@gmail.com>
2026-03-30 16:09:39 +0300

    fix: massive bug bash, all syntax and API URL interpolation errors resolved

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -8,9 +8,11 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3001',
+      'http://172.30.112.1:3001',
       'https://mogitech-rentos.vercel.app',
       'https://rentos.mogitechglobal.com',
     ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
